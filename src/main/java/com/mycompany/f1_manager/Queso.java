c1e81fef4aab12873b954b6640720e8f64827ead
Esteban1209 <84480286+Esteban1209@users.noreply.github.com>
2025-07-14 19:59:34 -0600

    Agrego nueva clase para test

---
+++ src/main/java/com/mycompany/f1_manager/Queso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.f1_manager;
+
+/**
+ *
+ * @author hp
+ */
+public class Queso {
+    
+}
